e71f9d6e878460af20917704667b37e2b27072e0
timojar <jarmalatimo@gmail.com>
2021-12-05 16:55:16 +0200

    Ag-grid added

--- carshop/src/Cars.js
+++ carshop/src/Cars.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 
 export default function Activities() {
   const [cars, setCars] = React.useState([]);
@@ -14,13 +17,26 @@ export default function Activities() {
       .catch((err) => console.log(err));
   }, []);
 
+  const columns = [
+    { headerName: "brand", field: "brand", sortable: true, filter: true },
+    { headerName: "color", field: "color", sortable: true, filter: true },
+    { headerName: "model", field: "model",  sortable: true, filter: true,},
+    { headerName: "fuel", field: "fuel",  sortable: true, filter: true,},
+    { headerName: "year", field: "year",  sortable: true, filter: true,},
+  ];
+
   return (
     <div>
-      {cars.map((c, index) => (
-        <p key={index}>
-          {c.brand} {c.color} {c.model}{c.fuel} {c.year}
-        </p>
-      ))}
+      <div
+        className="ag-theme-material"
+        style={{ height: "700px", width: "70%", margin: "auto" }}
+      >
+        <AgGridReact
+          rowSelection="single"
+          columnDefs={columns}
+          rowData={cars}
+        />
+      </div>
     </div>
   );
 }
